f7fe757eb080b466c0edfc498f43412be52d78b6
Reason Adhikari <72090971+adhikariReason@users.noreply.github.com>
2023-05-01 14:57:30 -0500

    changed path

--- credentials/database.py
+++ credentials/database.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, db
 
 # Initialize Firebase app
-cred = credentials.Certificate("credentials/socialmedia-f11a7-firebase-adminsdk-rnzba-d5ea840bf1.json")
+cred = credentials.Certificate("./socialmedia-f11a7-firebase-adminsdk-rnzba-d5ea840bf1.json")
 
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://socialmedia-f11a7-default-rtdb.firebaseio.com/'
